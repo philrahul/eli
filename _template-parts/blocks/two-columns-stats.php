96fd58396221b141e822672d28d630fc32b2cdfa
wayne.vibhor <wayne.vibhor@ithands.com>
2021-09-06 17:57:52 +0530

    added blocks for stats

--- _template-parts/blocks/two-columns-stats.php
+++ _template-parts/blocks/two-columns-stats.php
@@ -39,48 +39,65 @@ $link = get_field('link');
       <div class="row">
         <div class="col-12 col-md-5">
           <div class="content left">
+            <?php if(get_field('left_main_heading')):?>
             <h2 class="mb-0">
-              3x
+             <?php echo get_field('left_main_heading');?>
             </h2>
+            <?php endif;
+                if(get_field('left_main_description')):?>
             <div class="fs-normal">
-              Lorem ipsum dolor sit amet.
+                <?php echo get_field('left_main_description');?>
             </div>
+            <?php endif;?>
             <div class="row justify-content-between">
               <div class="col-12">
                 <div class="gradient-line-top position-relative w-100"></div>
               </div>
-              <div class="col-5 text-left">
+              <div class="col-12 col-lg-6 col-xl-5 text-left">
+              <?php if(get_field('inner_sub-heading_left')):?>
                 <h3 class="mb-0">
-                  90%
+                    <?php echo get_field('inner_sub-heading_left');?>
                 </h3>
+                <?php endif;
+                if(get_field('inner_description_left')):?>
                 <div class="fs-normal">
-                  Lorem ipsum dolor sit amet.
+                    <?php echo get_field('inner_description_left');?>
                 </div>
+                <?php endif;?>
               </div>
-              <div class="col-5 text-right">
+              <div class="col-12 col-lg-6 col-xl-5 text-right">
+                <?php if(get_field('inner_sub-heading_right')):?>
                 <h3 class="mb-0">
-                  18%
+                    <?php echo get_field('inner_sub-heading_right');?>
                 </h3>
+                <?php endif;
+                if(get_field('inner_description_right')):?>
                 <div class="fs-normal">
-                  Lorem ipsum dolor sit amet.
+                <?php echo get_field('inner_description_right');?>
                 </div>
+                <?php endif;?>
               </div>
             </div>
           </div>
         </div>
         <div class="col-12 col-md-7">
           <div class="content right">
+            <?php  if(get_field('right_main_heading')):?>
             <h2>
-              Opportunities of Hemp
+            <?php echo get_field('right_main_heading');?>
             </h2>
+            <?php endif;
+                if(get_field('right_main_description')):?>
             <div class="fs-normal">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tellus ipsum, interdum a fermentum id, aliquet eu ligula. Nulla fermentum ex ut facilisis egestas. Donec interdum sollicitudin rhoncus. Etiam quam nisi, sodales ut nisi ac, fringilla pellentesque nibh.
-              <br><br><br>
-              Donec interdum sollicitudin rhoncus. Etiam quam nisi, sodales ut nisi ac, fringilla pellentesque nibh.
+            <?php echo get_field('right_main_description');?>
             </div>
-            <a href="#" class="link-more fs-small">
-              Learn More <img src="./wp-content/themes/eli/assets/infg_arrow_right.svg" alt="">
+            <?php endif;
+                $link = get_field('link');
+                if($link):?>
+            <a href="<?php echo $link['url'];?>" class="link-more fs-small">
+            <?php echo $link['title'];?> <img src="./wp-content/themes/eli/assets/infg_arrow_right.svg" alt="">
             </a>
+            <?php endif;?>
           </div>
         </div>
       </div>
