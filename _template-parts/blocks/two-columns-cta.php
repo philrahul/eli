10b96e4e597373d7de70bd1dc6b7355461206110
philrahul <89982038+philrahul@users.noreply.github.com>
2021-09-06 18:00:22 +0530

    two-columns-cta reversed to empty state

--- _template-parts/blocks/two-columns-cta.php
+++ _template-parts/blocks/two-columns-cta.php
@@ -32,57 +32,8 @@ $description = get_field('description');
 $link = get_field('link');
 ?>
 <!-- banner section -->
-<section class="two-columns-section <?php echo esc_attr(implode(' ',$className)); ?> wh-section wh-section_two-columns-stats" id="<?php echo esc_attr($id); ?>">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md-5">
-          <div class="content left">
-            <h2 class="mb-0">
-              3x
-            </h2>
-            <div class="fs-normal">
-              Lorem ipsum dolor sit amet.
-            </div>
-            <div class="row justify-content-between">
-              <div class="col-12">
-                <div class="gradient-line-top position-relative w-100"></div>
-              </div>
-              <div class="col-5 text-left">
-                <h3 class="mb-0">
-                  90%
-                </h3>
-                <div class="fs-normal">
-                  Lorem ipsum dolor sit amet.
-                </div>
-              </div>
-              <div class="col-5 text-right">
-                <h3 class="mb-0">
-                  18%
-                </h3>
-                <div class="fs-normal">
-                  Lorem ipsum dolor sit amet.
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-7">
-          <div class="content right">
-            <h2>
-              Opportunities of Hemp
-            </h2>
-            <div class="fs-normal">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tellus ipsum, interdum a fermentum id, aliquet eu ligula. Nulla fermentum ex ut facilisis egestas. Donec interdum sollicitudin rhoncus. Etiam quam nisi, sodales ut nisi ac, fringilla pellentesque nibh.
-              <br><br><br>
-              Donec interdum sollicitudin rhoncus. Etiam quam nisi, sodales ut nisi ac, fringilla pellentesque nibh.
-            </div>
-            <a href="#" class="link-more fs-small">
-              Learn More <img src="./wp-content/themes/eli/assets/infg_arrow_right.svg" alt="">
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+<section class="two-columns-section <?php echo esc_attr(implode(' ',$className)); ?>" id="<?php echo esc_attr($id); ?>">
+    
 </section>
 <script>
    
